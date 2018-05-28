ae4b2b31b1c9740b7416965e90a18966d4895952
Steven <stevenxchung@gmail.com>
2018-05-28 16:59:56 -0400

    Add FizzBuzz exercise to Chapter 2

--- Coursework/Eloquent JS/02 - Program Structure.js
+++ Coursework/Eloquent JS/02 - Program Structure.js
@@ -1,7 +1,29 @@
 // Looping a triangle
-count = "";
-stringHash = "#######";
+var count = "";
+var stringHash = "#######";
 for (i = 0; i < stringHash.length; i++) {
   count += stringHash[i];
   console.log(count);
 }
+
+// FizzBuzz
+var nums = [...Array(101).keys()];
+// var nFizzBuzz = 0,
+//     nFizz = 0,
+//     nBuzz = 0;
+for (i = 1; i < nums.length; i++) {
+  if (i % 3 == 0 && i % 5 === 0) {
+    console.log("FizzBuzz");
+    // nFizzBuzz += 1;
+  } else if (i % 3 === 0) {
+    console.log("Fizz");
+    // nFizz += 1;
+  } else if (i % 5 === 0) {
+    console.log("Buzz");
+    // nBuzz += 1;
+  } else {
+    console.log(i);
+  }
+}
+
+// Chessboard
