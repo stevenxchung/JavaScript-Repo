173c50848a5aad359830cbb1aa9255ccede64348
Steven <stevenxchung@gmail.com>
2018-05-30 21:27:10 -0400

    Recursion exercise initial commit

--- Coursework/Eloquent JS/03 - Function.js
+++ Coursework/Eloquent JS/03 - Function.js
@@ -9,3 +9,19 @@ let min = (first, second) => {
 }
 
 min(1, 2)
+
+// Recursion
+let isEven = (n) => {
+  if (n === 0) {
+    return true;
+  } else if (n === 1) {
+    return false;
+  } else if (n < 0) {
+    return isEven(-n);
+  } else {
+    return isEven(n-2);
+  }
+}
+isEven(50);
+isEven(75);
+isEven(-1);
