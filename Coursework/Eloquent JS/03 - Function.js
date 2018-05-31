cb2a70598d023f2e986cf0f53d76fe3ad43c732b
Steven <stevenxchung@gmail.com>
2018-05-30 20:12:53 -0400

    Add commentary to minimum exercise

--- Coursework/Eloquent JS/03 - Function.js
+++ Coursework/Eloquent JS/03 - Function.js
@@ -1,5 +1,6 @@
 // Minimum
 let min = (first, second) => {
+  // If the first number is bigger return the other number else, return the first number
   if (first > second) {
     return second;
   } else {
