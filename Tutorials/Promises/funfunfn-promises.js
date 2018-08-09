d99d179d37d5fff1a7b77fe6d883e71ad46af7cf
Steven <stevenxchung@gmail.com>
2018-08-09 18:56:20 -0400

    Append example 4 - callback hell

--- Tutorials/Promises/funfunfn-promises.js
+++ Tutorials/Promises/funfunfn-promises.js
@@ -26,3 +26,25 @@ loadImageCallbacked('images/cat4.jpg', (error, img) => {
   imgElement.src = img.src;
   document.body.appendChild(imgElement);
 });
+
+// Example 4
+import loadImageCallbacked from './load-image-callbacked';
+
+let addImg = src => {
+  let imgElement = document.createElement('img');
+  imgElement.src = src;
+  document.body.appendChild(imgElement);
+};
+
+loadImageCallbacked('images/cat1.jpg', (error, img1) => {
+  if (error) throw error;
+  addImg(img1.src);
+  loadImageCallbacked('images/cat2.jpg', (error, img2) => {
+    if (error) throw error;
+    addImg(img2.src);
+    loadImageCallbacked('images/cat3.jpg', (error, img3) => {
+      if (error) throw error;
+      addImg(img3.src);
+    });
+  });
+});
