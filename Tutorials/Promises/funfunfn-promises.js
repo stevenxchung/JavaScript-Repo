7f03e9fcfbbcfb2664f1dc893906093d01cde2b2
Steven <stevenxchung@gmail.com>
2018-08-09 18:48:52 -0400

    Append example 2

--- Tutorials/Promises/funfunfn-promises.js
+++ Tutorials/Promises/funfunfn-promises.js
@@ -6,3 +6,14 @@ loadImagePromised('images/cat1.jpg').then(img => {
   imgElement.src = img.src;
   document.body.appendChild(imgElement);
 });
+
+// Example 2
+import loadImagePromised from './load-image-promised';
+
+let whenCatLoaded = loadImagePromised('images/cat3.jpg');
+
+whenCatLoaded.then(img => {
+  let imgElement = document.createElement('img');
+  imgElement.src = img.src;
+  document.body.appendChild(imgElement);
+});
