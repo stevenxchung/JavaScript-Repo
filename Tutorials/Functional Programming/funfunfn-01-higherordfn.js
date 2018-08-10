73c38b015c41919bd2e94dd67ffb2538dfc045d7
Steven <stevenxchung@gmail.com>
2018-08-10 19:26:20 -0400

    Append bare bones filter function

--- Tutorials/Functional Programming/funfunfn-01-higherordfn.js
+++ Tutorials/Functional Programming/funfunfn-01-higherordfn.js
@@ -1,6 +1,6 @@
 // Key Idea: Functions are values
 
-// Example
+// Example 1
 let triple = function(x) {
   return x * 3;
 };
@@ -8,3 +8,20 @@ let triple = function(x) {
 let waffle = triple;
 
 waffle(30);
+
+// Example 2
+let animals = [
+  { name: 'Bugs Bunny', species: 'rabbit' },
+  { name: 'Daffy Duck', species: 'duck' },
+  { name: 'Courage', species: 'dog' },
+  { name: 'Donald Duck', species: 'duck' }
+];
+
+let ducks = [];
+for (var i = 0; i < animals.length; i++) {
+  if (animals[i].species === 'duck') {
+    ducks.push(animals[i]);
+  }
+}
+
+console.log(ducks);
