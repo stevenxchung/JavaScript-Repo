35a20179b22889dde237658c825bc66b666ec9fe
Steven <stevenxchung@gmail.com>
2018-08-14 19:07:02 -0400

    Add example 2 to map

--- Tutorials/Functional Programming/funfunfn-02-map.js
+++ Tutorials/Functional Programming/funfunfn-02-map.js
@@ -11,6 +11,12 @@ let names = [];
 for (var i = 0; i < animals.length; i++) {
   names.push(animals[i].name);
 }
+// Check result
+console.log(names);
 
+// Example 2
+let names = animals.map(function(animal) {
+  return animal.name;
+});
 // Check result
 console.log(names);
