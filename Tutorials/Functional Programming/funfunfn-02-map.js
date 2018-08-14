c1031d553777c038927f506d7d5311881e936fbf
Steven <stevenxchung@gmail.com>
2018-08-14 19:05:01 -0400

    Add example 1 for map

---
+++ Tutorials/Functional Programming/funfunfn-02-map.js
@@ -0,0 +1,16 @@
+// Example 1
+let animals = [
+  { name: 'Bugs Bunny', species: 'rabbit' },
+  { name: 'Daffy', species: 'duck' },
+  { name: 'Donald', species: 'duck' },
+  { name: 'Goofy', species: 'dog' },
+  { name: 'Mickey', species: 'mouse' }
+];
+
+let names = [];
+for (var i = 0; i < animals.length; i++) {
+  names.push(animals[i].name);
+}
+
+// Check result
+console.log(names);
