38dd068762f6bc10a0a9528859958830c9c895be
Steven <stevenxchung@gmail.com>
2018-08-15 20:33:44 -0400

    Add example 1

--- Tutorials/Functional Programming/funfunfn-03-reduce.js
+++ Tutorials/Functional Programming/funfunfn-03-reduce.js
@@ -2,3 +2,16 @@
 // This lesson: reduce() can be used to express any list transformation e.g. map(), filter(), reject(), etc.
 
 // Example 1
+let orders = [
+    { amount: 250 },
+    { amount: 400 },
+    { amount: 100 },
+    { amount: 325 },
+]
+
+let totalAmount = 0;
+for (var i = 0; i < orders.length; i++) {
+    totalAmount += orders[i].amount;
+}
+// Check result
+console.log(totalAmount);
