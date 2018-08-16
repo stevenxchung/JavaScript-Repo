19b8a17ef3c41cc0305961a8bf1c0a0a20e4ec86
Steven <stevenxchung@gmail.com>
2018-08-15 20:26:30 -0400

    Initial commit

---
+++ Tutorials/Functional Programming/funfunfn-03-reduce.js
@@ -0,0 +1,4 @@
+// Recap: map(), filter(), and reject() transform a list into something else
+// This lesson: reduce() can be used to express any list transformation e.g. map(), filter(), reject(), etc.
+
+// Example 1
