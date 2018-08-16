41c656ecb343df07f4649b0b8dddf3ae50bbd0b6
Steven <stevenxchung@gmail.com>
2018-08-15 20:40:24 -0400

    Add reduce() for example 2

--- Tutorials/Functional Programming/funfunfn-03-reduce.js
+++ Tutorials/Functional Programming/funfunfn-03-reduce.js
@@ -3,15 +3,30 @@
 
 // Example 1
 let orders = [
-    { amount: 250 },
-    { amount: 400 },
-    { amount: 100 },
-    { amount: 325 },
-]
+  { amount: 250 },
+  { amount: 400 },
+  { amount: 100 },
+  { amount: 325 }
+];
 
 let totalAmount = 0;
 for (var i = 0; i < orders.length; i++) {
-    totalAmount += orders[i].amount;
+  totalAmount += orders[i].amount;
 }
+// Check result
+console.log(totalAmount);
+
+// Example 2
+let orders = [
+  { amount: 250 },
+  { amount: 400 },
+  { amount: 100 },
+  { amount: 325 }
+];
+
+let totalAmount = orders.reduce(function(sum, order) {
+    console.log("Current sum is: " + sum + " and order amount is: " + order.amount)
+  return sum + order.amount;
+}, 0);
 // Check result
 console.log(totalAmount);
