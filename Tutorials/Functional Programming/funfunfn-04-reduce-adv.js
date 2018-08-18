cecca954220a5d91bb9b3fcad167510eaa35b196
Steven <stevenxchung@gmail.com>
2018-08-18 12:23:00 -0400

    Initial commit

---
+++ Tutorials/Functional Programming/funfunfn-04-reduce-adv.js
@@ -0,0 +1,4 @@
+// Recap: reduce() can be used to express any list transformation e.g. map(), filter(), reject(), etc.
+// This lesson: reduce() is not limited to reducing arrays to numbers, it can do much more
+
+// Example 1
