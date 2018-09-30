8adf7a674339148b9f5b42de6da60d2f84e30edc
Steven <stevenxchung@gmail.com>
2018-09-30 10:29:59 -0400

    Merge two data streams together with mergeMap()

--- Projects/rxjs-playground/rxjs-playground.js
+++ Projects/rxjs-playground/rxjs-playground.js
@@ -37,5 +37,21 @@ let span = document.querySelector('span');
 //   .subscribe(event => (span.textContent = event.target.value));
 
 // let obs2 = rxjs
-//   .fromEvent(input1, 'input')
+//   .fromEvent(input2, 'input')
 //   .subscribe(event => (span.textContent = event.target.value));
+
+let obs1 = rxjs.fromEvent(input1, 'input');
+let obs2 = rxjs.fromEvent(input2, 'input');
+
+// Will merge two data streams together
+obs1
+  .pipe(
+    rxjs.operators.mergeMap(event1 => {
+      return obs2.pipe(
+        rxjs.operators.map(
+          event2 => event1.target.value + ' ' + event2.target.value
+        )
+      );
+    })
+  )
+  .subscribe(combinedValue => (span.textContent = combinedValue));
