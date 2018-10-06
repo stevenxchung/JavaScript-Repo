5123a21f46b9bb4f54bb29b7259b5a15a86cb925
Steven <stevenxchung@gmail.com>
2018-10-06 17:45:01 -0400

    Add forEach lesson

---
+++ Coursework/Egghead.io - Async Programming - The End of the Loop/array-forEach.js
@@ -0,0 +1,38 @@
+// Classic For Loop
+let getStockSymbols = stocks => {
+  let symbols = [],
+    counter,
+    stock;
+
+  for (counter = 0; counter < stocks.length; counter++) {
+    stock = stocks[counter];
+    symbols.push(stock.symbol);
+  }
+
+  return symbols;
+};
+
+let symbols = getStockSymbols([
+  { symbol: 'XFX', price: 240.22, volume: 23432 },
+  { symbol: 'TNZ', price: 332.19, volume: 234 },
+  { symbol: 'JXJ', price: 120.22, volume: 5323 }
+]);
+
+console.log(symbols);
+
+// forEach Method
+let getStockSymbols = stocks => {
+  let symbols = [];
+  stocks.forEach(stock => {
+    symbols.push(stock.symbol);
+  });
+  return symbols;
+};
+
+let symbols = getStockSymbols([
+  { symbol: 'XFX', price: 240.22, volume: 23432 },
+  { symbol: 'TNZ', price: 332.19, volume: 234 },
+  { symbol: 'JXJ', price: 120.22, volume: 5323 }
+]);
+
+console.log(symbols);
