9f0f4899b96323a3e752e36c49652f1a3393b328
Steven <stevenxchung@gmail.com>
2018-10-17 20:06:40 -0400

    Upgrade to async/await function

--- Coursework/Egghead.io - Async and Await/01-async-await-intro.js
+++ Coursework/Egghead.io - Async and Await/01-async-await-intro.js
@@ -1,14 +1,27 @@
 // Normal function
+// const fetch = require('node-fetch');
+
+// const showGitHubUser = handle => {
+//   const url = `https://api.github.com/users/${handle}`;
+//   fetch(url)
+//     .then(response => response.json())
+//     .then(user => {
+//       console.log(user.name);
+//       console.log(user.location);
+//     });
+// };
+
+// showGitHubUser('stevenxchung');
+
+// Async
 const fetch = require('node-fetch');
 
-const showGitHubUser = handle => {
+async const showGitHubUser = handle => {
   const url = `https://api.github.com/users/${handle}`;
-  fetch(url)
-    .then(response => response.json())
-    .then(user => {
-      console.log(user.name);
-      console.log(user.location);
-    });
+  const response = await fetch(url);
+  const user = await response.json();
+  console.log(user.name);
+  console.log(user.location);
 };
 
 showGitHubUser('stevenxchung');
