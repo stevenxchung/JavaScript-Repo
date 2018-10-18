4ee1c5943ea83681b80ecf8f72ee42442be85bd1
Steven <stevenxchung@gmail.com>
2018-10-18 18:17:18 -0400

    Add promise

--- Coursework/Egghead.io - Async and Await/02-async-promise.js
+++ Coursework/Egghead.io - Async and Await/02-async-promise.js
@@ -1,12 +1,26 @@
 // Initial async
+// const fetch = require('node-fetch');
+
+// const showGitHubUser = async handle => {
+//   const url = `https://api.github.com/users/${handle}`;
+//   const response = await fetch(url);
+//   const user = await response.json();
+//   console.log(user.name);
+//   console.log(user.location);
+// };
+
+// showGitHubUser('stevenxchung');
+
+// Async with promise
 const fetch = require('node-fetch');
 
 const showGitHubUser = async handle => {
   const url = `https://api.github.com/users/${handle}`;
   const response = await fetch(url);
-  const user = await response.json();
-  console.log(user.name);
-  console.log(user.location);
+  return await response.json();
 };
 
-showGitHubUser('stevenxchung');
+showGitHubUser('stevenxchung').then(user => {
+  console.log(user.name);
+  console.log(user.location);
+});
