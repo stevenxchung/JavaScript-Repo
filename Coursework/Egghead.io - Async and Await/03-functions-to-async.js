4d30a70bc84a029cdb265f7ee91025e301c67f4d
Steven <stevenxchung@gmail.com>
2018-10-20 13:18:33 -0400

    Convert showGitHubUser() to class and call via async

--- Coursework/Egghead.io - Async and Await/03-functions-to-async.js
+++ Coursework/Egghead.io - Async and Await/03-functions-to-async.js
@@ -11,3 +11,20 @@
 //   console.log(user.name);
 //   console.log(user.location);
 // });
+
+// Convert function to async
+const fetch = require('node-fetch');
+
+class GitHubApiClient {
+  async fetchUser(handle) {
+    const response = await fetch(url);
+    return await response.json();
+  }
+}
+
+(async () => {
+  const client = new GitHubApiClient();
+  const user = await client.fetchUser('stevenxchung');
+  console.log(user.name);
+  console.log(user.location);
+})();
