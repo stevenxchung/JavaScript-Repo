62d222b976c8a36fa2896d2a8634bca6afccd19b
Steven <stevenxchung@gmail.com>
2018-10-22 17:39:39 -0400

    Fix up syntax, change from awaiting two http requests sequentially to having performance being affected by the slower of the two requests

--- Coursework/Egghead.io - Async and Await/05-await-multi-promise.js
+++ Coursework/Egghead.io - Async and Await/05-await-multi-promise.js
@@ -1,4 +1,23 @@
-// Starting point
+// Sequential http requests
+// const fetch = require('node-fetch');
+
+// async function fetchFromGitHub(handle) {
+//   const url = `https://api.github.com/users/${handle}`;
+//   const response = await fetch(url);
+//   return await response.json();
+// }
+
+// async function showUserAndRepos(handle) {
+//   const user = await fetchFromGitHub(`/users/${handle}`);
+//   const repos = await fetchFromGitHub(`/users/${handle}/repos`);
+
+//   console.log(user.name);
+//   console.log(`${repos.length} repos`);
+// }
+
+// showUserAndRepos('stevenxchung');
+
+// Async with multiple promises
 const fetch = require('node-fetch');
 
 async function fetchFromGitHub(handle) {
@@ -8,8 +27,11 @@ async function fetchFromGitHub(handle) {
 }
 
 async function showUserAndRepos(handle) {
-  const user = await fetchFromGitHub(`/users/${handle}`);
-  const repos = await fetchFromGitHub(`/users/${handle}/repos`)
+  const userPromise = fetchFromGitHub(`/users/${handle}`);
+  const reposPromise = fetchFromGitHub(`/users/${handle}/repos`);
+
+  const user = await userPromise;
+  const repos = await reposPromise;
 
   console.log(user.name);
   console.log(`${repos.length} repos`);
