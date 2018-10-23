420e993a196f7e1f77bd487caf56762e9801706f
Steven <stevenxchung@gmail.com>
2018-10-23 18:04:38 -0400

    Initial commit

---
+++ Coursework/Egghead.io - Async and Await/06-await-promise-all.js
@@ -0,0 +1,14 @@
+// Sequential http requests
+const fetch = require('node-fetch');
+
+async function fetchFromGitHub(handle) {
+  const url = `https://api.github.com/users/${handle}`;
+  const response = await fetch(url);
+  return await response.json();
+}
+
+async function showUserAndRepos(handle) {
+
+}
+
+showUserAndRepos('stevenxchung');
