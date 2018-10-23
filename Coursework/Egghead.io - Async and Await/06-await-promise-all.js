7a2c90ed5b53633d59f0b98ed020bf80f98b4668
Steven <stevenxchung@gmail.com>
2018-10-23 18:16:52 -0400

    Refactor promise.all() using ES6 destructuring syntax

--- Coursework/Egghead.io - Async and Await/06-await-promise-all.js
+++ Coursework/Egghead.io - Async and Await/06-await-promise-all.js
@@ -1,4 +1,29 @@
 // Resolves after each promise within promise.all() is resolved
+// const fetch = require('node-fetch');
+
+// async function fetchFromGitHub(handle) {
+//   const url = `https://api.github.com/users/${handle}`;
+//   const response = await fetch(url);
+//   return await response.json();
+// }
+
+// async function showUserAndRepos(handle) {
+//   const results = Promise.all([
+//     fetchFromGitHub(`/users/${handle}`),
+//     fetchFromGitHub(`/users/${handle}/repos`),
+//   ])
+
+//   const user = results[0];
+//   const repos = results[1];
+
+//   console.log(user.name);
+//   console.log(`${repos.length} repos`);
+// }
+
+// showUserAndRepos('stevenxchung');
+
+// Refactor promise.all() using ES6 destructuring syntax
+// Reference: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Destructuring_assignment
 const fetch = require('node-fetch');
 
 async function fetchFromGitHub(handle) {
@@ -8,13 +33,10 @@ async function fetchFromGitHub(handle) {
 }
 
 async function showUserAndRepos(handle) {
-  const results = Promise.all([
+  const [user, repos] = Promise.all([
     fetchFromGitHub(`/users/${handle}`),
-    fetchFromGitHub(`/users/${handle}/repos`),
-  ])
-
-  const user = results[0];
-  const repos = results[1];
+    fetchFromGitHub(`/users/${handle}/repos`)
+  ]);
 
   console.log(user.name);
   console.log(`${repos.length} repos`);
