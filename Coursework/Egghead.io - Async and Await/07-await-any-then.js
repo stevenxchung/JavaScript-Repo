c6e05e0e21e519914bdf3388edb99e781f3b18d9
Steven <stevenxchung@gmail.com>
2018-10-24 19:00:34 -0400

    Import Bluebird which returns a thenable - object with a then() attached

--- Coursework/Egghead.io - Async and Await/07-await-any-then.js
+++ Coursework/Egghead.io - Async and Await/07-await-any-then.js
@@ -1,7 +1,19 @@
 // Await non-promise
+// async function main() {
+//   const x = await 42;
+//   console.log(x);
+// }
+
+// main();
+
+// Await with thenable
+const Bluebird = require('bluebird');
+
 async function main() {
-  const x = await 42;
-  console.log(x);
+  console.log('Working...');
+  // Returns a 'thenable' - an object with a then()
+  await Bluebird.delay(2000);
+  console.log('Done');
 }
 
 main();
