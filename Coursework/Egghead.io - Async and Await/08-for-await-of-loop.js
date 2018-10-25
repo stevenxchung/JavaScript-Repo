0af3a6be489572fcaa9482ea1ee7ccaf8c9bf925
Steven <stevenxchung@gmail.com>
2018-10-25 19:06:07 -0400

    Add async value generation

--- Coursework/Egghead.io - Async and Await/08-for-await-of-loop.js
+++ Coursework/Egghead.io - Async and Await/08-for-await-of-loop.js
@@ -1,7 +1,31 @@
 // Value generator
-function* someGenerator() {
+// function* someGenerator() {
+//   yield 1;
+//   yield 2;
+//   yield 3;
+// }
+
+// function main() {
+//   for (const value of someGenerator()) {
+//     console.log(value);
+//   }
+// }
+
+// main();
+
+// Async value generator
+Symbol.asyncIterator = Symbol.asyncIterator || Symbol('asyncIterator');
+
+const delay = (ms) => new Promise(resolve => {
+  setTimeout(resolve, ms);
+});
+
+async function* someGenerator() {
+  await delay(1000);
   yield 1;
+  await delay(1000);
   yield 2;
+  await delay(1000);
   yield 3;
 }
 
