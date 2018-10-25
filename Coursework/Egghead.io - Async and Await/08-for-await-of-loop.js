23720868e31adc1e42410e602d4d08c6dbf180e1
Steven <stevenxchung@gmail.com>
2018-10-25 19:12:31 -0400

    Add alternate way to implement async generator

--- Coursework/Egghead.io - Async and Await/08-for-await-of-loop.js
+++ Coursework/Egghead.io - Async and Await/08-for-await-of-loop.js
@@ -14,6 +14,31 @@
 // main();
 
 // Async value generator
+// Symbol.asyncIterator = Symbol.asyncIterator || Symbol('asyncIterator');
+
+// const delay = (ms) => new Promise(resolve => {
+//   setTimeout(resolve, ms);
+// });
+
+// async function* someGenerator() {
+//   await delay(1000);
+//   yield 1;
+//   await delay(1000);
+//   yield 2;
+//   await delay(1000);
+//   yield 3;
+// }
+
+// function main() {
+//   // Gets promise from someGenerator() and waits until promise has resolved
+//   for (const value of someGenerator()) {
+//     console.log(value);
+//   }
+// }
+
+// main();
+
+// Alternate way to implement async generator
 Symbol.asyncIterator = Symbol.asyncIterator || Symbol('asyncIterator');
 
 const delay = (ms) => new Promise(resolve => {
@@ -30,8 +55,14 @@ async function* someGenerator() {
 }
 
 function main() {
-  // Gets promise from someGenerator() and waits until promise has resolved
-  for (const value of someGenerator()) {
+  // Gets promise from someGenerator()
+  const generator = someGenerator();
+  while(true) {
+    // Await promise from generator.next() and deconstruct result into 'value' and 'done' property
+    const { value, done } = await generator.next();
+    if (done) {
+      break;
+    }
     console.log(value);
   }
 }
