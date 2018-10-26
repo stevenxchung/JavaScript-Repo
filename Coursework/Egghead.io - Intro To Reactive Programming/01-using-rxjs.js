9aba87665d579d35fd6cbbb7dbf52965b27b27ae
Steven <stevenxchung@gmail.com>
2018-10-26 19:29:55 -0400

    Using rxjs initial commit

---
+++ Coursework/Egghead.io - Intro To Reactive Programming/01-using-rxjs.js
@@ -0,0 +1,11 @@
+// What is Reactive Programming?
+// Reactive programming is programming with event streams
+
+// Example 1
+// Below is a program which console.log() the source array which is already in memory
+const example1 = (() => {
+  console.clear();
+  const source = ['1', '2', '3'];
+  const result = source;
+  console.log(result);
+})();
