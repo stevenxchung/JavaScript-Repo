e4bd3a271394dbc2ff21898c748ffe124e181603
Steven <stevenxchung@gmail.com>
2018-10-26 20:16:35 -0400

    Add map, filter, and reduce - synchronous

--- Coursework/Egghead.io - Intro To Reactive Programming/01-using-rxjs.js
+++ Coursework/Egghead.io - Intro To Reactive Programming/01-using-rxjs.js
@@ -1,20 +1,31 @@
 // What is Reactive Programming?
 // Reactive programming is programming with event streams
 
+console.clear();
+
 // Example 1
-// Below is a program which console.log() the source array which is already in memory
-// const example1 = (() => {
-//   console.clear();
-//   const source = ['1', '2', '3'];
-//   const result = source;
-//   console.log(result);
-// })();
+// Console log the source array which is already in memory
+const example1 = (() => {
+  const source = ['1', '2', '3'];
+  const result = source;
+  console.log('example 1:', result);
+})();
 
 // Example 2
-// Below is a program which will console.log the array values asynchronously in 500 ms intervals
+// Console log the array values asynchronously in 500 ms intervals
 const example2 = (() => {
-  console.clear();
   const source = rxjs.interval(500).pipe(rxjs.operators.take(6));
   //output: 0,1,2,3,4,5....
-  const subscribe = source.subscribe(val => console.log(val));
+  const subscribe = source.subscribe(val => console.log('example 2:', val));
+})();
+
+// Example 3
+// Map out the array to ints then filter out all non ints and return the sum
+const example3 = (() => {
+  const source = ['1', '2', '3', '4', '5', 'S', 't', 'e', 'v', 'e', 'n'];
+  const result = source
+    .map(x => parseInt(x))
+    .filter(x => !isNaN(x))
+    .reduce((x, y) => x + y);
+  console.log('example 3:', result);
 })();
