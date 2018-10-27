70c5f087a3d73d814f89e002aa0a9183e9ea13b3
Steven <stevenxchung@gmail.com>
2018-10-26 20:00:30 -0400

    Add async response

--- Coursework/Egghead.io - Intro To Reactive Programming/01-using-rxjs.js
+++ Coursework/Egghead.io - Intro To Reactive Programming/01-using-rxjs.js
@@ -3,9 +3,18 @@
 
 // Example 1
 // Below is a program which console.log() the source array which is already in memory
-const example1 = (() => {
+// const example1 = (() => {
+//   console.clear();
+//   const source = ['1', '2', '3'];
+//   const result = source;
+//   console.log(result);
+// })();
+
+// Example 2
+// Below is a program which will console.log the array values asynchronously in 500 ms intervals
+const example2 = (() => {
   console.clear();
-  const source = ['1', '2', '3'];
-  const result = source;
-  console.log(result);
+  const source = rxjs.interval(500).pipe(rxjs.operators.take(6));
+  //output: 0,1,2,3,4,5....
+  const subscribe = source.subscribe(val => console.log(val));
 })();
