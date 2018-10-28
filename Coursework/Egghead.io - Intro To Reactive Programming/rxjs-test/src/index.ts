26e7d2cef454c0984a5b6dd1923ea6e53b0fad5f
Steven <stevenxchung@gmail.com>
2018-10-28 18:04:47 -0400

    Example 1 successful

--- Coursework/Egghead.io - Intro To Reactive Programming/rxjs-test/src/index.ts
+++ Coursework/Egghead.io - Intro To Reactive Programming/rxjs-test/src/index.ts
@@ -13,39 +13,39 @@ const example1 = (() => {
   console.log('example 1:', result);
 })();
 
-// Example 2
-// Console log the array values asynchronously in 500 ms intervals
-const example2 = (() => {
-  const source = rxjs.interval(500).pipe(rxjs.operators.take(6));
-  //output: 0,1,2,3,4,5....
-  const subscribe = source.subscribe(val => console.log('example 2:', val));
-})();
+// // Example 2
+// // Console log the array values asynchronously in 500 ms intervals
+// const example2 = (() => {
+//   const source = rxjs.interval(500).pipe(rxjs.operators.take(6));
+//   //output: 0,1,2,3,4,5....
+//   const subscribe = source.subscribe(val => console.log('example 2:', val));
+// })();
 
-// Example 3
-// Map out the array to ints then filter out all non ints and return the sum
-const example3 = (() => {
-  const source = ['1', '2', '3', '4', '5', 'S', 't', 'e', 'v', 'e', 'n'];
-  const result = source
-    .map(x => parseInt(x))
-    .filter(x => !isNaN(x))
-    .reduce((x, y) => x + y);
-  console.log('example 3:', result);
-})();
+// // Example 3
+// // Map out the array to ints then filter out all non ints and return the sum
+// const example3 = (() => {
+//   const source = ['1', '2', '3', '4', '5', 'S', 't', 'e', 'v', 'e', 'n'];
+//   const result = source
+//     .map(x => parseInt(x))
+//     .filter(x => !isNaN(x))
+//     .reduce((x, y) => x + y);
+//   console.log('example 3:', result);
+// })();
 
-// Example 4
-// Same as example 3 but asynchronous
-const example4 = (() => {
-  const source = rxjs.pipe(
-    rxjs.Observable,
-    rxjs.interval(500),
-    rxjs.operators.take(11),
-    rxjs.operators.map(
-      i => ['1', '2', '3', '4', '5', 'S', 't', 'e', 'v', 'e', 'n'][i]
-    )
-  );
-  const result = source
-    .map(x => parseInt(x))
-    .filter(x => !isNaN(x))
-    .reduce((x, y) => x + y);
-  result.subscribe(x => console.log('example 4:', x));
-})();
+// // Example 4
+// // Same as example 3 but asynchronous
+// const example4 = (() => {
+//   const source = rxjs.pipe(
+//     rxjs.Observable,
+//     rxjs.interval(500),
+//     rxjs.operators.take(11),
+//     rxjs.operators.map(
+//       i => ['1', '2', '3', '4', '5', 'S', 't', 'e', 'v', 'e', 'n'][i]
+//     )
+//   );
+//   const result = source
+//     .map(x => parseInt(x))
+//     .filter(x => !isNaN(x))
+//     .reduce((x, y) => x + y);
+//   result.subscribe(x => console.log('example 4:', x));
+// })();
