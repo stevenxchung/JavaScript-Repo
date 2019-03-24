d64c58e6ef4d9e6d1231c8bd6e494c8279d6b5bc
Steven <stevenxchung@gmail.com>
2019-03-24 14:28:51 -0400

    Add methods and tests

--- Coursework/Egghead.io - Data Structures and Algorithms in JavaScript/02-queue.js
+++ Coursework/Egghead.io - Data Structures and Algorithms in JavaScript/02-queue.js
@@ -1,11 +1,46 @@
 // Queue Data Structure in JavaScript
 
-let createQueue() {
+let createQueue = () => {
+  const queue = [];
   return {
     // Add or enqueue
+    enqueue(item) {
+      queue.unshift(item);
+    },
     // Remove or dequeue
+    dequeue() {
+      return queue.pop();
+    },
     // Peek
+    peek() {
+      return queue[queue.length - 1];
+    },
     // Length
+    get length() {
+      return queue.length;
+    },
     // isEmpty
-  }
-}
+    isEmpty() {
+      return queue.length == 0;
+    }
+  };
+};
+
+// Tests
+const q = createQueue();
+console.log(q.isEmpty());
+
+q.enqueue('First');
+q.enqueue('Second');
+q.enqueue('Third');
+
+// Check out the last element (first element added)
+console.log(q.peek());
+// Remove the last item from the queue
+q.dequeue();
+console.log(q.peek());
+q.dequeue();
+console.log(q.peek());
+q.dequeue();
+
+console.log(q.isEmpty());
