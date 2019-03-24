f73af14bd64284d90dfe498e8f1553661a838c84
Steven <stevenxchung@gmail.com>
2019-03-24 18:52:40 -0400

    Create 03-priority-queue.js

---
+++ Coursework/Egghead.io - Data Structures and Algorithms in JavaScript/03-priority-queue.js
@@ -0,0 +1,17 @@
+// Priority Queue JavaScript Data Structure
+// We can make a priority queue by using two queues, a high and low priority queue
+
+const { createQueue } = require('./02-queue');
+
+let createPriorityQueue = () => {
+  const lowPriorityQueue = createQueue()
+  const highPriorityQueue = createQueue()
+
+  return {
+    // enqueue
+    // dequeue
+    // peek
+    // length
+    // isEmpty
+  }
+}
