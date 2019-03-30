88f95528dcc5cad828bcd97fcb580a5e7132feab
Steven <stevenxchung@gmail.com>
2019-03-30 11:24:06 -0400

    Update 03-priority-queue.js

--- Coursework/Egghead.io - Data Structures and Algorithms in JavaScript/03-priority-queue.js
+++ Coursework/Egghead.io - Data Structures and Algorithms in JavaScript/03-priority-queue.js
@@ -4,14 +4,51 @@
 const { createQueue } = require('./02-queue');
 
 let createPriorityQueue = () => {
-  const lowPriorityQueue = createQueue()
-  const highPriorityQueue = createQueue()
+  const lowPriorityQueue = createQueue();
+  const highPriorityQueue = createQueue();
 
   return {
-    // enqueue
-    // dequeue
-    // peek
-    // length
+    // Enqueue
+    enqueue(item, isHighPriority = false) {
+      isHighPriority
+        ? highPriorityQueue.enqueue(item)
+        : lowPriorityQueue.enqueue(item);
+    },
+    // Dequeue
+    dequeue() {
+      if (!highPriorityQueue.isEmpty()) {
+        return highPriorityQueue.dequeue();
+      }
+
+      return lowPriorityQueue.dequeue();
+    },
+    // Peek
+    peek() {
+      if (!highPriorityQueue.isEmpty()) {
+        return highPriorityQueue.peek();
+      }
+
+      return lowPriorityQueue.peek();
+    },
+    // Length
+    length() {
+      return highPriorityQueue.length + lowPriorityQueue.length;
+    },
     // isEmpty
-  }
-}
+    isEmpty() {
+      return highPriorityQueue.isEmpty() && lowPriorityQueue.isEmpty();
+    }
+  };
+};
+
+// Tests
+const q = createPriorityQueue();
+
+q.enqueue('First');
+q.enqueue('Second');
+q.enqueue('Third');
+
+q.dequeue();
+q.enqueue('Code Red!', true);
+console.log(q.dequeue());
+console.log(q.peek());
