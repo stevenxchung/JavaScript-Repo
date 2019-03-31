5dfd88188e0f37435709b19a7f907e3219b54703
Steven <stevenxchung@gmail.com>
2019-03-31 12:52:46 -0400

    Create 05-linked-list.js

---
+++ Coursework/Egghead.io - Data Structures and Algorithms in JavaScript/05-linked-list.js
@@ -0,0 +1,20 @@
+// Linked Lists Data Structure in JavaScript
+
+let createNode = value => {
+  return {
+    value,
+    next: null
+  };
+};
+
+let createLinkedList = () => {
+  return {
+    // Head
+    // Tail
+    // Length
+    // Push
+    // Pop
+    // Delete
+    // isEmpty
+  };
+};
