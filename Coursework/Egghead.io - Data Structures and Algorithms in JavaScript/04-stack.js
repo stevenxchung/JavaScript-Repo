7a19cc50b49459c31769e7dd934ee4dd73a493c0
Steven <stevenxchung@gmail.com>
2019-03-31 08:22:06 -0400

    Define methods and add tests

--- Coursework/Egghead.io - Data Structures and Algorithms in JavaScript/04-stack.js
+++ Coursework/Egghead.io - Data Structures and Algorithms in JavaScript/04-stack.js
@@ -1,11 +1,48 @@
 // Stack Data Structure in JavaScript
 
 let createStack = () => {
+  const array = [];
+
   return {
-    // push
-    // pop
-    // peek
-    // length
+    // Push
+    push(item) {
+      array.push(item);
+    },
+    // Pop
+    pop() {
+      return array.pop();
+    },
+    // Peek
+    peek() {
+      return array[array.length - 1];
+    },
+    // Length
+    get length() {
+      return array.length;
+    },
     // isEmpty
-  }
-}
+    isEmpty() {
+      return array.length == 0;
+    }
+  };
+};
+
+// Tests
+const s = createStack();
+
+s.push(1);
+s.push(2);
+s.push(3);
+s.push(4);
+s.push(5);
+
+console.log(s.peek());
+s.pop();
+console.log(s.peek());
+console.log(s.length);
+s.pop();
+s.pop();
+s.pop();
+s.pop();
+console.log(s.peek());
+console.log(s.length);
