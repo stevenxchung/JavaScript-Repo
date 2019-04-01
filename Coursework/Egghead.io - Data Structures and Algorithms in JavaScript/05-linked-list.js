a878fd9157858754fd71f8e4fe2efe369cf30804
Steven <stevenxchung@gmail.com>
2019-04-01 18:23:33 -0400

    Define methods and add tests

--- Coursework/Egghead.io - Data Structures and Algorithms in JavaScript/05-linked-list.js
+++ Coursework/Egghead.io - Data Structures and Algorithms in JavaScript/05-linked-list.js
@@ -10,11 +10,133 @@ let createNode = value => {
 let createLinkedList = () => {
   return {
     // Head
+    head: null,
     // Tail
+    tail: null,
     // Length
+    length: 0,
     // Push
+    push(value) {
+      const node = createNode(value);
+
+      if (this.head == null) {
+        this.head = node;
+        this.tail = node;
+        this.length++;
+        return node;
+      }
+
+      this.tail.next = node;
+      this.tail = node;
+      this.length++;
+
+      return node;
+    },
     // Pop
+    pop() {
+      if (this.isEmpty()) {
+        return null;
+      }
+
+      const node = this.tail;
+
+      if (this.head == this.tail) {
+        this.head = null;
+        this.tail = null;
+        this.length--;
+        return node;
+      }
+
+      let current = this.head;
+      let penultimate;
+      while (current) {
+        if (current.next == this.tail) {
+          penultimate = current;
+          break;
+        }
+
+        current = current.next;
+      }
+
+      penultimate.next = null;
+      this.tail = penultimate;
+      this.length--;
+
+      return node;
+    },
+    // Get
+    get(index) {
+      if (index < 0 || index > this.length - 1) {
+        return null;
+      }
+
+      if (index == 0) {
+        return this.head;
+      }
+
+      let current = this.head;
+      let i = 0;
+
+      while (i < index) {
+        i++;
+        current = current.next;
+      }
+
+      return current;
+    },
     // Delete
+    delete(index) {
+      if (index < 0 || index > this.length - 1) {
+        return null;
+      }
+
+      if (index == 0) {
+        const deleted = this.head;
+
+        this.head = this.head.next;
+        this.length--;
+
+        return deleted;
+      }
+
+      let current = this.head;
+      let previous;
+      let i = 0;
+
+      while (i < index) {
+        i++;
+        previous = current;
+        current = current.next;
+      }
+
+      const deleted = current;
+      previous.next = current.next;
+      this.length--;
+
+      return deleted;
+    },
     // isEmpty
+    isEmpty() {
+      return this.length == 0;
+    },
+    print() {
+      const values = [];
+      let current = this.head;
+
+      while (current) {
+        values.push(current.value);
+        current = current.next;
+      }
+
+      return values.join(' => ');
+    }
   };
 };
+
+// Tests
+const list = createLinkedList();
+const values = ['a', 'b', 'c', 'd', 'e'];
+const nodes = values.map(val => list.push(val));
+
+console.log(list.get(4));
+console.log(list.print());
