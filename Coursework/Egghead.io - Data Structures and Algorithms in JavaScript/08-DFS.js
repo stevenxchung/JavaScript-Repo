eacefa726f5d3297c64f2269f2626d698afdb548
stevenxchung <stevenxchung@gmail.com>
2019-04-28 12:39:38 -0400

    Create 08-DFS.js

---
+++ Coursework/Egghead.io - Data Structures and Algorithms in JavaScript/08-DFS.js
@@ -0,0 +1,90 @@
+// Depth First Search Algorithm for Graphs in JavaScript
+
+const { createQueue } = require('../queues/index.js');
+
+function createNode(key) {
+  const children = [];
+
+  return {
+    key,
+    children,
+    addChild(node) {
+      children.push(node);
+    }
+  };
+}
+
+function createGraph(directed = false) {
+  const nodes = [];
+  const edges = [];
+
+  return {
+    directed,
+    nodes,
+    edges,
+
+    addNode(key) {
+      nodes.push(createNode(key));
+    },
+
+    getNode(key) {
+      return nodes.find(n => n.key === key);
+    },
+
+    addEdge(node1Key, node2Key) {
+      const node1 = this.getNode(node1Key);
+      const node2 = this.getNode(node2Key);
+
+      node1.addChild(node2);
+
+      if (!directed) {
+        node2.addChild(node1);
+      }
+
+      edges.push(`${node1Key}${node2Key}`);
+    },
+
+    print() {
+      return nodes
+        .map(({ children, key }) => {
+          let result = `${key}`;
+
+          if (children.length) {
+            result += ` => ${children.map(node => node.key).join(' ')}`;
+          }
+
+          return result;
+        })
+        .join('\n');
+    },
+
+    // Breadth First Search
+    breadthFirstSearch(startingNodeKey, visitFn) {
+      const startingNode = this.getNode(startingNodeKey);
+      const visited = nodes.reduce((acc, node) => {
+        acc[node.key] = false;
+        return acc;
+      }, {});
+      const queue = createQueue();
+      queue.enqueue(startingNode);
+
+      while (!queue.isEmpty()) {
+        const currentNode = queue.dequeue();
+
+        if (!visited[currentNode.key]) {
+          visitFn(currentNode);
+          visited[currentNode.key] = true;
+        }
+
+        currentNode.neighbors.forEach(node => {
+          if (!visited[node.key]) {
+            queue.enqueue(node);
+          }
+        });
+      }
+    },
+
+    // Depth First Search
+    
+  };
+}
