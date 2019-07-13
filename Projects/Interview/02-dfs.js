285633013b95690d21ecf329d3d16917bcc63ccd
Steven C <stevenxchung@gmail.com>
2019-07-13 10:28:44 -0400

    Update 02-dfs.js

--- Projects/Interview/02-dfs.js
+++ Projects/Interview/02-dfs.js
@@ -4,3 +4,16 @@ let executeDFS = (graph, source) => {
   let stack = [];
   return nodesTraversed;
 };
+
+// Test
+const graph = [
+  [1], // 0
+  [0, 4, 5], // 1
+  [3, 4, 5], // 2
+  [2, 6], // 3
+  [1, 2], // 4
+  [1, 2, 6], // 5
+  [3, 5], // 6
+  [] // 7
+];
+console.log(executeDFS(graph, 0)); // [0, 1, 4, 2, 3, 6, 5]
