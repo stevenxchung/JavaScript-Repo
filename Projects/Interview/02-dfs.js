88cb6eb88176677e6c3de02955f524a160416e85
Steven C <stevenxchung@gmail.com>
2019-07-13 10:26:40 -0400

    Create 02-dfs.js

---
+++ Projects/Interview/02-dfs.js
@@ -0,0 +1,6 @@
+// Given a graph and source, returns an array of node numbers in DFS order
+let executeDFS = (graph, source) => {
+  let nodesTraversed = [];
+  let stack = [];
+  return nodesTraversed;
+};
