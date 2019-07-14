c757aadbe048ce1a3911195573d35a3a6e1d4fce
Steven C <stevenxchung@gmail.com>
2019-07-14 15:42:46 -0400

    Update 02-dfs.js

--- Projects/Interview/02-dfs.js
+++ Projects/Interview/02-dfs.js
@@ -2,6 +2,25 @@
 let executeDFS = (graph, source) => {
   let nodesTraversed = [];
   let stack = [];
+  // Push source to the array
+  stack.push(source);
+  // Continue until stack is empty
+  while (stack.length > 0) {
+    // Remove first node in stack each time
+    let currentNode = stack.pop();
+    for (let i = 0; i < graph[currentNode].length; i++) {
+      let neighbor = graph[currentNode][i];
+      if (!nodesTraversed.includes(neighbor)) {
+        console.log('current node', currentNode);
+        console.log('neighbor', neighbor);
+        stack.push(neighbor);
+        if (!nodesTraversed.includes(currentNode)) {
+          // Only push current node to nodesTraversed array once
+          nodesTraversed.push(currentNode);
+        }
+      }
+    }
+  }
   return nodesTraversed;
 };
 
@@ -16,4 +35,4 @@ const graph = [
   [3, 5], // 6
   [] // 7
 ];
-console.log(executeDFS(graph, 0)); // [0, 1, 4, 2, 3, 6, 5]
+console.log(executeDFS(graph, 0)); // [0, 1, 5, 6, 3, 2]
