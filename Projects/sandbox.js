30367fa0e9a9affd3d6f60ae45c6d85fc969d34b
Steven <stevenxchung@gmail.com>
2019-12-16 21:11:37 -0500

    Create sandbox.js

---
+++ Projects/sandbox.js
@@ -0,0 +1,22 @@
+const accounts = [
+  {
+    accountId: '001',
+    accountType: 'TnE',
+    roleName: 'Admin'
+  },
+  {
+    accountId: '002',
+    accountType: 'TnE_CARDHOLDER',
+    roleName: 'Cardholder'
+  },
+  {
+    accountId: '003',
+    accountType: 'EAP',
+    roleName: 'EAP Admin Role 001'
+  },
+  {
+    accountId: '004',
+    accountType: 'EAP',
+    roleName: 'EAP Admin Role 002'
+  }
+];
