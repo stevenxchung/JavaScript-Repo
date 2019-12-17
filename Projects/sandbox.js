06308505f2bbc236e6b97d39d484f617a451c6a0
Steven <stevenxchung@gmail.com>
2019-12-16 21:33:45 -0500

    Update sandbox.js

--- Projects/sandbox.js
+++ Projects/sandbox.js
@@ -1,28 +1,46 @@
 const accounts = [
   {
     accountId: '001',
-    accountType: 'TnE',
+    accountType: 'First',
     roleName: 'Admin'
   },
   {
     accountId: '002',
-    accountType: 'TnE_CARDHOLDER',
+    accountType: 'First - Alternative',
     roleName: 'Cardholder'
   },
   {
     accountId: '003',
-    accountType: 'EAP',
-    roleName: 'EAP Admin Role 001'
+    accountType: 'Second',
+    roleName: 'Secondary Admin Role 001'
   },
   {
     accountId: '004',
-    accountType: 'EAP',
-    roleName: 'EAP Admin Role 002'
+    accountType: 'Second',
+    roleName: 'Secondary Admin Role 002'
   }
 ];
+
+const productNames = ['Knights', 'Dragons'];
+
+const productIcon = ['K', 'D'];
 
 // export interface Product = {
 //     productName: string
 //     productIcon: string
 //     accounts: Accounts[]
 // };
+
+let createProductMap = accounts => {
+  const accountsFirst = accounts.filter(account =>
+    account.accountType.toLowerCase().includes('first')
+  );
+  const accountsSecond = accounts.filter(account =>
+    account.accountType.toLowerCase().includes('second')
+  );
+
+  // console.log({ accountsFirst, accountsSecond });
+};
+
+// Testing
+console.log(createProductMap(accounts));
