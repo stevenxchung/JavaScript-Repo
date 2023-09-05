12cee69f2a2a41a509dbe4de648a04fb5709c61b
Steven <stevenxchung@gmail.com>
2023-09-05 19:18:32 -0400

    LC 167

--- Sandbox/lc-with-test-suite.js
+++ Sandbox/lc-with-test-suite.js
@@ -1,36 +1,47 @@
 const { performance } = require("perf_hooks");
 
 class Solution {
-  method(nums) {
+  method(numbers, target) {
     /*
-    convert input to set
-    check if element start of sequence
-    increment and check set -> update longest sequence
+    use two pointers at ends of input array
+    loop until target is found
     */
-    const seen = new Set(nums);
-    let longestSequence = 0;
-    for (const num of seen) {
-      if (seen.has(num - 1)) continue;
+    let [l, r] = [0, numbers.length - 1];
 
-      let count = 1;
-      while (seen.has(num + count)) {
-        count++;
-      }
-      longestSequence = Math.max(longestSequence, count);
+    while (l < r) {
+      const sum = numbers[l] + numbers[r];
+      if (sum === target) return [l + 1, r + 1];
+      else if (sum < target) l++; // Go towards bigger numbers
+      else r--; // Go towards smaller numbers
     }
-
-    return longestSequence;
   }
 
-  reference() {}
+  reference(numbers, target) {
+    let [left, right] = [0, numbers.length - 1];
+
+    while (left < right) {
+      const sum = numbers[left] + numbers[right];
+
+      const isTarget = sum === target;
+      if (isTarget) return [left + 1, right + 1];
+
+      const isTargetGreater = sum < target;
+      if (isTargetGreater) left++;
+
+      const isTargetLess = target < sum;
+      if (isTargetLess) right--;
+    }
+
+    return [-1, -1];
+  }
 
   quantify(testCases, runs = 1e6) {
     const runsArr = Array.from({ length: runs });
     const solStart = performance.now();
     runsArr.map((_, i) => {
       testCases.map((input) => {
-        if (i === 0) console.log(this.method(input));
-        else this.method(input);
+        if (i === 0) console.log(this.method(...input));
+        else this.method(...input);
       });
     });
     console.log(
@@ -40,8 +51,8 @@ class Solution {
     const refStart = performance.now();
     runsArr.map((_, i) => {
       testCases.map((input) => {
-        if (i === 0) console.log(this.reference(input));
-        else this.reference(input);
+        if (i === 0) console.log(this.reference(...input));
+        else this.reference(...input);
       });
     });
     console.log(
@@ -52,7 +63,8 @@ class Solution {
 
 const test = new Solution();
 const testCases = [
-  [100, 4, 200, 1, 3, 2],
-  [0, 3, 7, 2, 5, 8, 4, 6, 0, 1],
+  [[2, 7, 11, 15], 9],
+  [[2, 3, 4], 6],
+  [[-1, 0], -1],
 ];
 test.quantify(testCases);
