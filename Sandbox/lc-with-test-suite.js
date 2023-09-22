2ae0d668df99edf7d5d8bd11320c8b5e055949e8
Steven <stevenxchung@gmail.com>
2023-09-22 10:20:35 -0400

    LC 22

--- Sandbox/lc-with-test-suite.js
+++ Sandbox/lc-with-test-suite.js
@@ -1,32 +1,28 @@
 const { performance } = require("perf_hooks");
 
 /*
-  - k = max pile is always doable within the given hours so k = [1... max pile]
-  - Instead of searching linearly [1... max pile], apply binary search on k range
-  - Iterate over piles for each choice of k and compare time to finish
+  - Use DFS to generate all possible combinations of parenthesis
+  - Add "(" first and only add ")" if there are more ")" than "("
 */
 class Solution {
-  method(piles, h) {
-    // Choices for k are from [1... max pile]
-    let res = Math.max(...piles);
-    let [l, r] = [1, res];
-    while (l <= r) {
-      let k = Math.floor((l + r) / 2);
+  method(n) {
+    const res = [];
 
-      let timeToFinish = 0;
-      for (const p of piles) {
-        // Time to finish a pile = bananas in a pile over k rounded up
-        timeToFinish += Math.ceil(p / k);
+    const dfs = (open, close, combo) => {
+      if (open === 0 && close === 0) {
+        res.push(combo);
+        return;
       }
 
-      if (timeToFinish <= h) {
-        res = Math.min(res, k);
-        r = k - 1;
-      } else {
-        l = k + 1;
-      }
-    }
+      // Prefer to add "(" first if available
+      if (open > 0) dfs(open - 1, close, combo + "(");
+      // Only add ")" parenthesis if there are more ")" than "("
+      if (close > open) dfs(open, close - 1, combo + ")");
 
+      return;
+    };
+
+    dfs(n, n, "");
     return res;
   }
 
@@ -37,8 +33,8 @@ class Solution {
     const solStart = performance.now();
     runsArr.map((_, i) => {
       testCases.map((input) => {
-        if (i === 0) console.log(this.method(...input));
-        else this.method(...input);
+        if (i === 0) console.log(this.method(input));
+        else this.method(input);
       });
     });
     console.log(
@@ -48,8 +44,8 @@ class Solution {
     const refStart = performance.now();
     runsArr.map((_, i) => {
       testCases.map((input) => {
-        if (i === 0) console.log(this.reference(...input));
-        else this.reference(...input);
+        if (i === 0) console.log(this.reference(input));
+        else this.reference(input);
       });
     });
     console.log(
@@ -59,11 +55,5 @@ class Solution {
 }
 
 const test = new Solution();
-const testCases = [
-  [[3, 6, 7, 11], 8],
-  [[30, 11, 23, 4, 20], 5],
-  [[30, 11, 23, 4, 20], 6],
-  // Additional
-  [[312884470], 312884469],
-];
+const testCases = [3, 1];
 test.quantify(testCases);
