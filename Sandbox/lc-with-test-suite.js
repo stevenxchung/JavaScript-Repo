3dd33139324ab504a51d09536e744f1f367f7563
Steven <stevenxchung@gmail.com>
2023-10-12 09:00:20 -0400

    LC 230

--- Sandbox/lc-with-test-suite.js
+++ Sandbox/lc-with-test-suite.js
@@ -9,23 +9,27 @@ class TreeNode {
 }
 
 /*
-  - DFS to determine if subtree is BST
-  - A tree with a single node is a BST
-  - Treat left or right value as previous node value
-  - Left and right nodes must be strictly < or > than current node
+  - Use in-order DFS to traverse tree
+  - Track values in an array and return when array length = k
+  - The kth smallest value is the last value in the array
 */
 class Solution {
-  method(root) {
-    const isBST = (node, left, right) => {
-      if (!node) return true;
-      if (left >= node.val || node.val >= right) return false;
+  method(root, k) {
+    const res = [];
 
-      return (
-        isBST(node.left, left, node.val) && isBST(node.right, node.val, right)
-      );
+    const dfs = (node) => {
+      if (!node || res.length === k) return;
+
+      dfs(node.left);
+      // Only add to array if length < k
+      if (res.length < k) res.push(node.val);
+      dfs(node.right);
+
+      return;
     };
 
-    return isBST(root, -Number.MAX_VALUE, Number.MAX_VALUE);
+    dfs(root);
+    return res.pop();
   }
 
   reference() {}
@@ -35,8 +39,8 @@ class Solution {
     const solStart = performance.now();
     runsArr.map((_, i) => {
       testCases.map((input) => {
-        if (i === 0) console.log(this.method(input));
-        else this.method(input);
+        if (i === 0) console.log(this.method(...input));
+        else this.method(...input);
       });
     });
     console.log(
@@ -46,8 +50,8 @@ class Solution {
     const refStart = performance.now();
     runsArr.map((_, i) => {
       testCases.map((input) => {
-        if (i === 0) console.log(this.reference(input));
-        else this.reference(input);
+        if (i === 0) console.log(this.reference(...input));
+        else this.reference(...input);
       });
     });
     console.log(
@@ -58,11 +62,14 @@ class Solution {
 
 const test = new Solution();
 const testCases = [
-  new TreeNode(2, new TreeNode(1), new TreeNode(3)),
-  new TreeNode(
-    5,
-    new TreeNode(1),
-    new TreeNode(4, new TreeNode(3), new TreeNode(6))
-  ),
+  [new TreeNode(3, new TreeNode(1, null, new TreeNode(2)), new TreeNode(4)), 1],
+  [
+    new TreeNode(
+      5,
+      new TreeNode(3, new TreeNode(2, new TreeNode(1)), new TreeNode(4)),
+      new TreeNode(6)
+    ),
+    3,
+  ],
 ];
 test.quantify(testCases);
